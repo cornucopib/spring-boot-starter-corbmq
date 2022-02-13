30c3f5e97cd9bb64065383c298cb821dd13d6e40
Cornucopib <13040839537@163.com>
2022-02-14 00:25:01 +0800

    Initial commit

---
+++ spring-boot-starter-corbmq-core/src/main/java/com/cornucopib/corbmq/starter/autoconfigure/CorbMQAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.cornucopib.corbmq.starter.autoconfigure;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * Corb自动配置类.
+ *
+ * @author cornucopib
+ * @since 2022/2/14
+ */
+@Configurable
+public class CorbMQAutoConfiguration {
+
+}
